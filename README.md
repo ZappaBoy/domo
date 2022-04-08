61d54e87304daca25e4eed17c8f41d39216ab45b
zappaboy <federico_zappone@hotmail.it>
2022-04-08 20:18:14 +0200

    Changed script structure

--- README.md
+++ README.md
@@ -5,3 +5,35 @@
 ```shell
 install domo.py ~/.local/bin/domo
 ```
+
+# Config file
+
+Create a `config.json` in `DOMO_ROOT` (default `~/.config/domo/`)
+
+```json
+{
+  "broadlink": {
+    "manager_url": "",
+    "command_endpoint": "",
+    "devices": [
+      {
+        "name": "",
+        "type": "",
+        "ip": "",
+        "mac": "",
+        "commands": {
+          "volume_up": "",
+          "volume_down": "",
+          "mute": "",
+          "unmute": "",
+          "sound_on": "",
+          "sound_off": ""
+        }
+      }
+    ]
+  },
+  "switchbot": {
+    "token": ""
+  }
+}
+```
--- domo.py
+++ domo.py
@@ -1,55 +1,55 @@
 #!/bin/python
 
 import base64
+import json
 import os
 import sys
 
 import requests
 
-manager_url = 'http://localhost:7020/'
-command_url = manager_url + 'command/send/'
 
-device_type = os.environ.get('BL_DEVICE_TYPE')
-device_ip = os.environ.get('BL_DEVICE_IP')
-device_mac = os.environ.get('BL_DEVICE_MAC')
+class Domo:
+    config_home = os.environ.get('DOMO_ROOT', os.path.expanduser('~') + '/.config/domo/')
+    config_path = os.path.join(config_home + 'config.json')
 
-soundbar_volume_up = "JgBIAAABLJUUERURFREUERURFREUEhQ2FDYUNhI4FDUVNRU1FTUVERQRFREVERQ2FBEVERURFBIUNhQ2FDYUERU1FTUVNRU2FAANBQAAAAAAAAAAAAAAAAAA"
-soundbar_volume_down = "JgCQAAABLZQUERURFREUEhQSFBEVERU1FTYUNhQ2FDYUNhQ2FDYUEhQ2FBEVNhQ2FREUEhQSFBIUEhQ2FREVERQ2FDYUNhQ2FAAFKAABL5QSExURFBIUEhUQFREVERU1EzcTNxM3EzcSOBI4EjgSExQ2ExMUNhI4EhMVERQRFREVERU1FREUEhQ2FDYUNhQ2FAANBQAAAAAAAA=="
-soundbar_mute = "JgCQAAABLpMUERURFREVERQSFBEVERU1FTUVNRU1FTUVNRU1FTUUEhQRFTUVNRU1FTUVERQSFBEVNRURExMUERURFTUVNRU1FQAFKAABL5MVERQRFREVERQSFBEVERU1FTUVNRU1FTYUNhQ2FDYUEhQRFTUVNRU1FTUVERQSFBIUNhQRFREVERURFDYUNhU1FQANBQAAAAAAAA=="
-soundbar_power = "JgCQAAABLZMVERQRFREVERURFBIUERU2FDYUNRU2FDYUNhQ2FDUVERURFDYUEhQRFTYUERURFBIUNhQSFDYUNhQRFTUVNRU1FQAFJwABL5MVERQRFREVERQRFREVERU1FTUUNhQ2FDYUNhQ2FDUVERURFDYUERURFTUVERQSFBEVNRURFDYUNhQRFTYUNRU1FQANBQAAAAAAAA=="
+    f = open(config_path)
+    config = json.load(f)
 
-commands = {
-    'volume_up': soundbar_volume_up,
-    'volume_down': soundbar_volume_down,
-    'mute': soundbar_mute,
-    'unmute': soundbar_mute,
-    'sound_on': soundbar_power,
-    'sound_off': soundbar_power
-}
+    broadlink_config = config['broadlink']
+    manager_url = broadlink_config['manager_url']
+    command_url = manager_url + broadlink_config['command_endpoint']
 
-if not device_type or not device_ip or not device_mac:
-    print('Device environment variables not defined')
-    exit(1)
+    switchbot_token = config['switchbot']['token']
 
-if len(sys.argv) <= 0:
-    print('Command not defined')
-    exit(1)
+    def main(self):
+        if len(sys.argv) <= 0:
+            print('Command not defined')
+            exit(1)
 
-command_to_send = sys.argv[1]
+        device = sys.argv[1]
+
+        if device == 'switchbot':
+            command_to_send = ''
+            commands = ''
+        else:
+            command_to_send = sys.argv[2]
+            device_config = self.broadlink_config['devices'][device]
+            commands = device_config['commands']
+
+            for command_key in commands.keys():
+                if command_to_send == command_key:
+                    self.send_command(device_config, commands[command_to_send])
+
+    def send_command(self, device_config, command):
+        params = {
+            'type': device_config['type'],
+            'host': device_config['ip'],
+            'mac': device_config['mac'],
+            'command': (base64.b64decode(command)).hex()
+        }
+        requests.get(url=self.command_url, params=params)
+        print('Done')
 
 
-def send_command(command):
-    params = {
-        'type': device_type,
-        'host': device_ip,
-        'mac': device_mac,
-        'command': (base64.b64decode(command)).hex()
-    }
-
-    r = requests.get(url=command_url, params=params)
-    print(r.json())
-
-
-for command_key in commands.keys():
-    if command_to_send == command_key:
-        send_command(commands[command_to_send])
+if __name__ == '__main__':
+    Domo().main()
