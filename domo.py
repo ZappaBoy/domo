7279a7f949005540624f1d7224941a3d0be173db
zappaboy <federico_zappone@hotmail.it>
2022-10-02 15:05:38 +0200

    Added switchbot support

--- domo.py
+++ domo.py
@@ -1,11 +1,23 @@
 #!/bin/python
 
 import base64
+import hashlib
+import hmac
 import json
 import os
 import sys
+import time
 
 import requests
+
+
+def get_switchbot_auth_params(token, secret, nonce):
+    t = int(round(time.time() * 1000))
+    string_to_sign = '{}{}{}'.format(token, t, nonce)
+    string_to_sign = bytes(string_to_sign, 'utf-8')
+    secret = bytes(secret, 'utf-8')
+    sign = base64.b64encode(hmac.new(secret, msg=string_to_sign, digestmod=hashlib.sha256).digest())
+    return str(sign, 'utf-8'), str(t), nonce
 
 
 class Domo:
@@ -18,8 +30,23 @@ class Domo:
     broadlink_config = config['broadlink']
     manager_url = broadlink_config['manager_url']
     command_url = manager_url + broadlink_config['command_endpoint']
+    broadlink_devices = broadlink_config['devices']
 
-    switchbot_token = config['switchbot']['token']
+    switchbot_config = config['switchbot']
+    switchbot_devices = switchbot_config['devices']
+    switchbot_base_url = switchbot_config['base_url']
+    sign, t, nonce = get_switchbot_auth_params(
+        switchbot_config['token'],
+        switchbot_config['secret'],
+        switchbot_config['nonce']
+    )
+    switchbot_headers = {
+        'Content-type': 'application/json; charset=utf8',
+        'Authorization': switchbot_config['token'],
+        'sign': sign,
+        't': t,
+        'nonce': nonce
+    }
 
     def main(self):
         if len(sys.argv) <= 0:
@@ -27,28 +54,42 @@ class Domo:
             exit(1)
 
         device = sys.argv[1]
+        command = sys.argv[2]
 
-        if device == 'switchbot':
-            command_to_send = ''
-            commands = ''
-        else:
-            command_to_send = sys.argv[2]
-            device_config = self.broadlink_config['devices'][device]
-            commands = device_config['commands']
+        if device in self.broadlink_devices:
+            self.send_broadlink_command(self.broadlink_devices[device], command)
 
-            for command_key in commands.keys():
-                if command_to_send == command_key:
-                    self.send_command(device_config, commands[command_to_send])
+        if device in self.switchbot_devices:
+            self.send_switchbot_command(self.switchbot_devices[device], command)
 
-    def send_command(self, device_config, command):
-        params = {
-            'type': device_config['type'],
-            'host': device_config['ip'],
-            'mac': device_config['mac'],
-            'command': (base64.b64decode(command)).hex()
-        }
-        requests.get(url=self.command_url, params=params)
-        print('Done')
+    def send_broadlink_command(self, device_config, command):
+        commands = device_config['commands']
+        if command in commands:
+            params = {
+                'type': device_config['type'],
+                'host': device_config['ip'],
+                'mac': device_config['mac'],
+                'command': (base64.b64decode(commands[command])).hex()
+            }
+            requests.get(url=self.command_url, params=params)
+
+    def send_switchbot_command(self, device_config, command):
+        device_id = device_config['device_id']
+        commands = device_config['commands']
+        if command in device_config['commands']:
+            command_url = self.switchbot_base_url + '/v1.1/devices/' + device_id + '/commands'
+            data = {
+                "command": commands[command],
+                "parameter": "default",
+                "commandType": "command"
+            }
+            requests.post(url=command_url, headers=self.switchbot_headers, data=json.dumps(data))
+
+    def get_switchbot_devices(self):
+        # Check documentation for more info: https://github.com/OpenWonderLabs/SwitchBotAPI#get-device-list
+        devices = requests.get(url=self.switchbot_base_url + '/v1.1/devices', headers=self.switchbot_headers)
+        devices = devices.json()['body']
+        print(devices)
 
 
 if __name__ == '__main__':
